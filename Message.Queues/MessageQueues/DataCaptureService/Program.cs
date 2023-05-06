9c2792e5e78768838c48f611282633e138e2311e
Gezerus <gezer20101992@gmail.com>
2023-05-06 09:20:23 +0200

    Completed Task 3

---
+++ Message.Queues/MessageQueues/DataCaptureService/Program.cs
@@ -0,0 +1,44 @@
+﻿using MessageQueueLib;
+using System;
+using System.IO;
+
+namespace DataCaptureService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                Path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Input"),
+                Filter = "*.*"
+            };
+            watcher.Created += new FileSystemEventHandler(FileCopied);
+            watcher.EnableRaisingEvents = true;
+            Console.ReadLine();
+        }
+
+        private static void FileCopied(object sender, FileSystemEventArgs e)
+        {
+            WaitForCopying(e.FullPath);
+            DocumentQueue.Send(e.FullPath, e.Name);
+        }
+
+        private static void WaitForCopying(string path)
+        {
+            var count = 0;
+            while (true) 
+            {
+                try
+                {
+                    using (File.Open(path, FileMode.Open, FileAccess.ReadWrite))
+                    break;
+                }
+                catch (IOException)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ Message.Queues/MessageQueues/MessageQueueLib/FileMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MessageQueueLib
+{
+    public class FileMessage
+    {
+        public Guid Id { get; set; }
+
+        public int Position { get; set; }
+
+        public int Count { get; set; }
+
+        public string Name { get; set; }
+
+        public byte[] Body { get; set; }
+
+        public static FileMessage Create(Guid id, string name, byte[] bytes)
+        {
+            return new FileMessage
+            {
+                Id = id,
+                Name = name,
+                Body = bytes
+            };
+        }
+    }
+}
